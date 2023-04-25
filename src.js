c804218ac1f98218ac9c2df12456d323f536c5e1
manikandan <116661624+Ak9500@users.noreply.github.com>
2023-04-25 18:13:13 +0530

    Add files via upload

---
+++ src.js
@@ -0,0 +1,7 @@
+var arr = ["guvi", 'geek', 'zen', 'fullstack'];
+var ano = function(arr) {
+ for (var i = 0; i <= arr.length; i++) {
+ console.log(arr[i][0].toUpperCase(0) + arr[i].substr(0));
+ }
+}
+ console.log(arr)
